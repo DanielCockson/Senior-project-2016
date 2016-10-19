b88f424d126b73800dceda6d5cafb33033933663
DanielCockson <djcockson@gmail.com>
2016-10-19 09:17:59 -0400

    Add files via upload

---
+++ Limit_Switch_test/Limit_Switch_test.ino
@@ -0,0 +1,34 @@
+void loop() {
+    static enum {MOVING_CW, MOVING_CCW, STOPPED_CW, STOPPED_CCW} state;
+    static unsigned long time_stopped;
+    unsigned long now = millis();
+
+    switch (state) {
+    case MOVING_CW:
+        if (digitalRead(LM2) == LOW) {  // hit switch
+            digitalWrite(CW, LOW);      // stop
+            state = STOPPED_CW;
+            time_stopped = now;
+        }
+        break;
+    case MOVING_CCW:
+        if (digitalRead(LM1) == LOW) {  // hit switch
+            digitalWrite(CCW, LOW);     // stop
+            state = STOPPED_CCW;
+            time_stopped = now;
+        }
+        break;
+    case STOPPED_CW:
+        if (now - time_stopped >= 5000) {
+            digitalWrite(CCW, HIGH);    // restart
+            state = MOVING_CCW;
+        }
+        break;
+    case STOPPED_CCW:
+        if (now - time_stopped >= 5000) {
+            digitalWrite(CW, HIGH);     // restart
+            state = MOVING_CW;
+        }
+        break;
+    }
+}
---
+++ Stepper_Test_code/Stepper_Test_code.ino
@@ -0,0 +1,22 @@
+//#incluee <AccelStepper.h>
+#include <Wire.h> 
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMStop (0x60);
+Adafruit_StepperMotor *myMotor = AFMStop.getStepper(200, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+AFMStop.begin();
+myMotor->setSpeed(200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+myMotor->step(200, FORWARD, DOUBLE); //(two hundred steps, forward, ???)
+delay(1000); //(milliseconds?)
+myMotor->step(400, BACKWARD, DOUBLE);
+}
